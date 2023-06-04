92655a7d40d8fe751c7fbc9dedaa670e7092be8a
Shwetank Aryan <aayanrobox02@gmail.com>
2023-06-04 11:34:48 +0530

    contactFrom finalized

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -38,9 +38,7 @@ function ContactForm() {
         <div className="w-1/2 mx-auto my-4 pt-16">
             <h2 className="text-2xl text-white font-bold mb-4">
             <button className="rounded-full shadow shadow-slate-700 font-bold bg-teal-600 p-3 text-2xl">
-                    <Link to="/contact_form">
                         Create Contact
-                    </Link>
                 </button>
                 </h2>
             <div className="mb-4">
